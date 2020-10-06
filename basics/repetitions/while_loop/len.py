b81cb9ebd237e02c3dfc934db38ec3167ccd53e1
Ismail K <ismailkefokeris@gmail.com>
2020-10-06 12:08:52 +0100

    Activity 4 and 5 (not complete)

---
+++ basics/repetitions/while_loop/len.py
@@ -0,0 +1,10 @@
+
+print("Please enter a phrase:")
+phrase = str(input())
+
+num = 0
+
+#prints the word bop the same number of  times depending on the amount of characters in "phrase"
+while (num != len(phrase)):
+    print("Bop")
+    num += 1
---
+++ basics/repetitions/while_loop/sum_100.py
@@ -0,0 +1,16 @@
+
+#dont really understand Activity 5: sum 100 Week 2...
+print ("Calculating the sum of the first 100 numbers...")
+
+num = 100
+num2 = 0
+num3 = 1
+final = 0
+
+while (num2 != num):
+    num3 = num3 + num
+    final = num3
+    num2 += 1
+    print (num3)
+
+print ("Done ", final)
