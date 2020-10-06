d578de019936e10e659c19aa7d7886a578a8ef08
Ismail K <ismailkefokeris@gmail.com>
2020-10-06 09:25:40 +0100

    Added code to demonstrate the use of an IF statement

---
+++ basics/Decisions/simple_decision/if.py
@@ -0,0 +1,9 @@
+
+book = str(input("What book genre do you like? "))
+
+if (book == "adventure"):
+    print ("Oh boy do I love {} books".format(book))
+else:
+    print ("Thats veryy interesting, personally i like adventure books ")
+
+print ("Finished reading book.")
