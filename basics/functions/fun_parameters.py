286fdc5f2e0e162a786a3dd88c979954607f0e06
Ismail K <ismailkefokeris@gmail.com>
2020-10-13 10:38:14 +0100

    Multiple parameters in functions

---
+++ basics/functions/fun_parameters.py
@@ -0,0 +1,12 @@
+
+def climb_ladder(remain, passed):
+
+    if (remain > passed):
+        print ("Still a wayyy to goo ;-;")
+    elif (remain < passed):
+        print ("Woo almost there")
+    else:
+        print ("well welll wellllllwell....ERrrorr>>....Breaking DO.w..n....")
+    
+climb_ladder(5, 2)
+climb_ladder(2, 5)
